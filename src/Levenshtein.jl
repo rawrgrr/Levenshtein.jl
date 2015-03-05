7ef2aa7d3a6710b67ae1c511c21fbeb332dc6d4a
Utkarsh Upadhyay <musically.ut@gmail.com>
2015-03-05 10:04:45 +0100

    Utilize a O(min(source, target)) space complexity algorithm.
     - Rename parameters
     - Add tests
     - Test the new implementation against the original base implementation

--- src/Levenshtein.jl
+++ src/Levenshtein.jl
@@ -4,39 +4,59 @@
 module Levenshtein
 export levenshtein
 
-function levenshtein(a::String, b::String)
-    return levenshtein(a, b, 1)
+function levenshtein(source::String, target::String)
+    return levenshtein(source, target, 1)
 end
 
-function levenshtein(a::String, b::String, cost::Real)
-    return levenshtein(a, b, cost, cost, cost)
+function levenshtein(source::String, target::String, cost::Real)
+    return levenshtein(source, target, cost, cost, cost)
 end
 
-function levenshtein(a::String, b::String, deletion_cost::Real, insertion_cost::Real, substitution_cost::Real)
-    local costs::Array{Real, 2} = zeros(Real, length(a) + 1, length(b) + 1)
-    # Initialize the deletion costs
-    for i in 1:length(a)
-        costs[i + 1,1] = i * deletion_cost
-    end
-    # Initialize the insertion costs
-    for i in 1:length(b)
-        costs[1, i + 1] = i * insertion_cost
-    end
+function levenshtein(source::String, target::String, deletion_cost::Real, insertion_cost::Real, substitution_cost::Real)
+    if length(source) < length(target)
+        # Space complexity of function = O(length(target))
+        return levenshtein(target, source, insertion_cost, deletion_cost, substitution_cost)
+    else
+        if length(source) == 0
+            return length(target) * insertion_cost
+        elseif length(target) == 0
+            return length(source) * deletion_cost
+        else
+            local oldRow::Array{Real, 1} = zeros(Real, length(target) + 1)
+            local newRow::Array{Real, 1} = zeros(Real, length(target) + 1)
 
-    local row = 2
-    for r in a
-        local col = 2
-        for c in b
-            local deletion::Real = costs[row - 1, col] + deletion_cost
-            local insertion::Real = costs[row, col - 1] + insertion_cost
-            local substitution::Real = costs[row - 1, col - 1] + (r == c ? 0 : substitution_cost)
-            costs[row, col] = min(deletion, insertion, substitution)
-            col += 1
+            # Initialize the old row for empty source and i characters in target
+            oldRow[1] = 0
+            for i in 1:length(target)
+                oldRow[i + 1] = i * insertion_cost
+            end
+
+            i = 0
+            for r in source 
+                i += 1
+
+                # Delete i characters from source to get empty target
+                newRow[1] = i * deletion_cost
+
+                j = 0
+                for c in target 
+                    j += 1
+
+                    local deletion::Real = oldRow[j + 1] + deletion_cost
+                    local insertion::Real = newRow[j] + insertion_cost
+                    local substitution::Real = oldRow[j] + (r == c ? 0 : substitution_cost)
+
+                    newRow[j + 1] = min(deletion, insertion, substitution)
+                end
+
+                for k in 1:(length(target) + 1)
+                    oldRow[k] = newRow[k]
+                end
+            end
+
+            return newRow[length(target) + 1]
         end
-        row += 1
     end
-
-    return costs[length(a) + 1, length(b) + 1]
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,78 @@
 using Levenshtein
 using Base.Test
 
-# write your own tests here
+function levenshtein_base(a::String, b::String, deletion_cost::Real, insertion_cost::Real, substitution_cost::Real)
+    local costs::Array{Real, 2} = zeros(Real, length(a) + 1, length(b) + 1)
+    # Initialize the deletion costs
+    for i in 1:length(a)
+        costs[i + 1,1] = i * deletion_cost
+    end
+    # Initialize the insertion costs
+    for i in 1:length(b)
+        costs[1, i + 1] = i * insertion_cost
+    end
+
+    local row = 2
+    for r in a
+        local col = 2
+        for c in b
+            local deletion::Real = costs[row - 1, col] + deletion_cost
+            local insertion::Real = costs[row, col - 1] + insertion_cost
+            local substitution::Real = costs[row - 1, col - 1] + (r == c ? 0 : substitution_cost)
+            costs[row, col] = min(deletion, insertion, substitution)
+            col += 1
+        end
+        row += 1
+    end
+
+    return costs[length(a) + 1, length(b) + 1]
+end
+
+# Testing base implementation
 # Tests checked with http://www.kurzhals.info/static/samples/levenshtein_distance/
-@test 1 == 1
+@test 4 == levenshtein_base("Hi, my name is", "my name is", 1, 1, 1)
+@test 21 == levenshtein_base("%^@!^@#^@#!! Snoooooooop", "Dro!p it!!!! like it's hot", 1, 1, 1)
+@test 7 == levenshtein_base("Alborgów", "amoniak", 1, 1, 1)
+
+@test 3 == levenshtein_base("sitting", "kitten", 1, 1, 1)
+@test 1 == levenshtein_base("sitting", "kitten", 1, 0, 0)
+@test 0 == levenshtein_base("sitting", "kitten", 0, 1, 0)
+@test 0 == levenshtein_base("sitting", "kitten", 0, 0, 1)
+@test 5 == levenshtein_base("sitting", "kitten", 1, 1, Inf)
+@test 12 == levenshtein_base("sitting", "kitten", 10, 1, 1)
+
+
+# write your own tests here
 @test 4 == levenshtein("Hi, my name is", "my name is")
 @test 21 == levenshtein("%^@!^@#^@#!! Snoooooooop", "Dro!p it!!!! like it's hot")
 @test 7 == levenshtein("Alborgów", "amoniak")
 
+@test 3 == levenshtein("sitting", "kitten")
+@test 1 == levenshtein("sitting", "kitten", 1, 0, 0)
+@test 0 == levenshtein("sitting", "kitten", 0, 1, 0)
+@test 0 == levenshtein("sitting", "kitten", 0, 0, 1)
+@test 5 == levenshtein("sitting", "kitten", 1, 1, Inf)
+@test 12 == levenshtein("sitting", "kitten", 10, 1, 1)
+
+@test levenshtein("a", "ab", 1, 2, 0) == levenshtein("ab", "a", 2, 1, 0)
+
+@test levenshtein("a", "", 10, 0, 0) == 10
+@test levenshtein("", "a", 0, 10, 0) == 10
+
+function randString(len)
+    return utf8(string([char(int(rand() * 255 + 1)) for _ in 1:len]...))
+end
+
+for i in 1:100
+    s = randString(floor(rand() * 100))
+    t = randString(floor(rand() * 100))
+    ins, del, sub = rand() * 1000, rand() * 1000, rand() * 1000
+
+    calculated, truth = levenshtein(s, t, del, ins, sub), levenshtein_base(s, t, del, ins, sub)
+
+    if calculated != truth
+        println("s = $s, t = $t, ins = $ins, del = $del, sub = $sub, calculated = $calculated, truth = $truth")
+    end
+
+    @test abs(calculated - truth) < 1e-9
+end
