4a190f72a302792f474856971d452f6292229181
Roger Tu <rtu@cal.berkeley.edu>
2015-07-03 17:46:53 -0700

    Performance improvements
    - changing ternary to explicit if-statment
    - changing array copy to pointer switch
    - adding support for re-using a matrix to store intermediate values

--- src/Levenshtein.jl
+++ src/Levenshtein.jl
@@ -2,7 +2,10 @@
 # Simple module for measuring distance between 2 strings
 ##
 module Levenshtein
+
 export levenshtein
+export levenshtein!
+
 
 function levenshtein(source::String, target::String)
     return levenshtein(source, target, 1)
@@ -13,49 +16,61 @@ function levenshtein(source::String, target::String, cost::Real)
 end
 
 function levenshtein{R<:Real,S<:Real,T<:Real}(source::String, target::String, deletion_cost::R, insertion_cost::S, substitution_cost::T)
+    return levenshtein!(target, source, insertion_cost, deletion_cost, substitution_cost)
+end
+
+function levenshtein!{R<:Real,S<:Real,T<:Real}(
+    source::String,
+    target::String,
+    deletion_cost::R,
+    insertion_cost::S,
+    substitution_cost::T,
+    costs::Matrix = Array(promote_type(R, S, T), 2, length(target) + 1)
+)
     cost_type = promote_type(R,S,T)
     if length(source) < length(target)
         # Space complexity of function = O(length(target))
-        return levenshtein(target, source, insertion_cost, deletion_cost, substitution_cost)
+        return levenshtein!(target, source, insertion_cost, deletion_cost, substitution_cost, costs)
     else
         if length(source) == 0
             return length(target) * insertion_cost
         elseif length(target) == 0
             return length(source) * deletion_cost
         else
-            oldRow = zeros(cost_type, length(target) + 1)
-            newRow = zeros(cost_type, length(target) + 1)
+            old_cost_index = 1
+            new_cost_index = 2
 
-            # Initialize the old row for empty source and i characters in target
-            oldRow[1] = 0
+            costs[old_cost_index, 1] = 0
             for i in 1:length(target)
-                oldRow[i + 1] = i * insertion_cost
+                costs[old_cost_index, i + 1] = i * insertion_cost
             end
 
             i = 0
-            for r in source 
+            for r in source
                 i += 1
 
                 # Delete i characters from source to get empty target
-                newRow[1] = i * deletion_cost
+                costs[new_cost_index, 1] = i * deletion_cost
 
                 j = 0
-                for c in target 
+                for c in target
                     j += 1
 
-                    deletion = oldRow[j + 1] + deletion_cost
-                    insertion = newRow[j] + insertion_cost
-                    substitution = oldRow[j] + (r == c ? 0 : substitution_cost)
+                    deletion = costs[old_cost_index, j + 1] + deletion_cost
+                    insertion = costs[new_cost_index, j] + insertion_cost
+                    substitution = costs[old_cost_index, j]
+                    if r != c
+                        substitution += substitution_cost
+                    end
 
-                    newRow[j + 1] = min(deletion, insertion, substitution)
+                    costs[new_cost_index, j + 1] = min(deletion, insertion, substitution)
                 end
 
-                for k in 1:(length(target) + 1)
-                    oldRow[k] = newRow[k]
-                end
+                old_cost_index, new_cost_index = new_cost_index, old_cost_index
             end
 
-            return newRow[length(target) + 1]
+            new_cost_index = old_cost_index
+            return costs[new_cost_index, length(target) + 1]
         end
     end
 end
---
+++ test/performance.jl
@@ -0,0 +1,43 @@
+using Levenshtein
+
+function randString(len)
+    return utf8(string([char(int(rand() * 255 + 1)) for _ in 1:len]...))
+end
+
+function run_huge_string(l1, l2)
+    # setup long string against short one to expose element copy performance
+    s = randString(l1)
+    t = randString(l2)
+    ins, del, sub = rand() * 1000, rand() * 1000, rand() * 1000
+
+    # run a long string against a not so long one
+    println("\nRunning $l1-char String against $l2-char String")
+    @time levenshtein(s, t, ins, del, sub)
+end
+
+function run_multiple_times(l1, l2, runs)
+    # setup
+    s = randString(l1)
+    t = randString(l2)
+    ins, del, sub = rand() * 1000, rand() * 1000, rand() * 1000
+    costs = Array(Float64, 2, max(length(s), length(t)) + 1)
+
+    # run without reusing cost matrix
+    println("\nRunning $runs runs without reusing cost matrix for $l1-char String vs $l2-char String")
+    @time for i in 1:runs
+        levenshtein(s, t, ins, del, sub)
+    end
+
+    # run without allocating cost matrix everytime
+    println("\nRunning $runs runs with resuable cost matrix for $l1-char String vs $l2-char String")
+    @time for i in 1:runs
+        levenshtein!(s, t, ins, del, sub, costs)
+    end
+end
+
+run_huge_string(10^7, 10^2)
+run_huge_string(10^6, 10^3)
+run_huge_string(10^5, 10^4)
+run_multiple_times(10^3, 10^3, 10^3)
+run_multiple_times(10^2, 10^2, 10^5)
+run_multiple_times(10^1, 10^1, 10^7)
