99de2a5333bf71ed3e23c45a5198cc0e3a80ac9d
Venkatesh Kumar <vrkumar@ucsd.edu>
2017-07-18 16:50:59 -0700

    Updated to remove 0.6 deprecation warning

--- src/Levenshtein.jl
+++ src/Levenshtein.jl
@@ -27,7 +27,7 @@ function levenshtein!{R<:Real,S<:Real,T<:Real}(
     deletion_cost::R,
     insertion_cost::S,
     substitution_cost::T,
-    costs::Matrix = Array(promote_type(R, S, T), 2, length(target) + 1)
+    costs::Matrix = Array{promote_type(R, S, T)}( 2, length(target) + 1)
 )
     cost_type = promote_type(R,S,T)
     if length(source) < length(target)
